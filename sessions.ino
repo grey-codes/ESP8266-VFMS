f6260e21f86a93966eeda88d5790ce33e38b37ec
Grey Ruessler <greyruessler@gmail.com>
2019-11-10 00:44:11 -0600

    ready data for hashing

--- sessions.ino
+++ sessions.ino
@@ -1,9 +1,14 @@
+#include <time.h>
+
 void session_init() {
 	if (server.hasHeader("Cookie")) {
 		DBG_OUTPUT_PORT.println("Found cookie:");
 		String cookie = server.header("Cookie");
 		DBG_OUTPUT_PORT.println(cookie);
 	} else {
+        String ua = server.hasHeader("User-Agent") ? server.header("User-Agent") : "noagent";
+        time_t t=time(NULL);
+        String hashIn = String(t) + ua;
 		server.sendHeader("Set-Cookie", "test=cookie");
 	}
 }
