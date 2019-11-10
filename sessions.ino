35bbc2cec7e6fb5e618156094036e2c0ace66eb7
Grey Ruessler <greyruessler@gmail.com>
2019-11-10 00:19:24 -0600

    let's use \t like humans should

--- sessions.ino
+++ sessions.ino
@@ -1,9 +1,9 @@
 void session_init() {
-  if (server.hasHeader("Cookie")) {
-    DBG_OUTPUT_PORT.println("Found cookie:");
-    String cookie = server.header("Cookie");
-    DBG_OUTPUT_PORT.println(cookie);
-  } else {
-    server.sendHeader("Set-Cookie", "test=cookie");
-  }
+	if (server.hasHeader("Cookie")) {
+		DBG_OUTPUT_PORT.println("Found cookie:");
+		String cookie = server.header("Cookie");
+		DBG_OUTPUT_PORT.println(cookie);
+	} else {
+		server.sendHeader("Set-Cookie", "test=cookie");
+	}
 }
