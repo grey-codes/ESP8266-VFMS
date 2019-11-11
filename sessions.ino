c246f472e4b98a177090a23ec69b4a791bcb8d25
Grey Ruessler <greyruessler@gmail.com>
2019-11-10 21:06:01 -0600

    session ID properly extracted from cookies string

--- sessions.ino
+++ sessions.ino
@@ -1,10 +1,26 @@
 #include <time.h>
 
-void session_init() {
-	if (server.hasHeader("Cookie")) {
-		DBG_OUTPUT_PORT.println("Found cookie:");
-		String cookie = server.header("Cookie");
-		DBG_OUTPUT_PORT.println(cookie);
+String session_id() {
+    if (!server.hasHeader("Cookie"))
+        return "";
+    String cookies =  server.header("Cookie");
+    int sessID_pos = cookies.indexOf("sessID");
+    if (sessID_pos==-1)
+        return "";
+    int start = cookies.indexOf("sessID=")+7;
+    if (start==6)
+        return "";
+    int end = cookies.indexOf(";",start);
+    if (end==-1) {
+        return cookies.substring(start);
+    }
+    return cookies.substring(start,end);
+}
+
+String session_init() {
+    String sessID = session_id();
+	if (sessID.length()>0) {
+		return sessID;
 	} else {
         String ua = server.hasHeader("User-Agent") ? server.header("User-Agent") : "noagent";
         time_t t=time(NULL);
@@ -15,8 +31,11 @@ void session_init() {
         md5.add(ts);
         md5.add(ua);
         md5.calculate();
+        String md5s = md5.toString();
 
-        DBG_OUTPUT_PORT.println(md5.toString());
-		server.sendHeader("Set-Cookie", "sessID=" + md5.toString());
+        DBG_OUTPUT_PORT.println("Initializing session: " + md5s);
+		server.sendHeader("Set-Cookie", "sessID=" + md5s);
+
+        return md5s;
 	}
 }
