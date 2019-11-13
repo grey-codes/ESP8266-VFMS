13b0c4b11eb647de4a257695f18e4f4b60d5b154
Grey Ruessler <greyruessler@gmail.com>
2019-11-13 02:24:10 -0600

    integrated working access control on index

--- data/btnControl.js
+++ data/btnControl.js
@@ -1,42 +1,43 @@
-function btnControl() {
-const USERGROUP = 3;	
-if (USERGROUP & 1) {
+function btnControl(usergroup) {
+	if (usergroup & 1) {
 
-	if (output0State = "off") {
-		$("#bluebtn").attr('class', 'button');
-	} else {
-		$("#bluebtn").attr('class', 'button2');
+		//if (output0State == "off") {
+			$("#bluebtn").attr('class', 'button');
+		//} else {
+		//	$("#bluebtn").attr('class', 'button2');	
+		//}
 		
+	} else {
+			$("#bluebtn").attr('class', 'btnhide');
 	}
-	
-} else {
-		$("#bluebtn").attr('class', 'btnhide');
+
+
+	if (usergroup & 2) {
+
+		//if (output4State == "off") {
+			$("#greenbtn").attr('class', 'button');
+		//} else {
+		//	$("#greenbtn").attr('class', 'button2');
+		//}
+		
+	} else {
+			$("#greenbtn").attr('class', 'btnhide');
+	}
+
+	if (usergroup & 4) {
+		//if (output5State == "off") {
+			$("#redbtn").attr('class', 'button');
+		//} else {
+		//	$("#redbtn").attr('class', 'button2');
+		//}
+		
+	} else {
+			$("#redbtn").attr('class', 'btnhide');
+	}
 }
 
-
-if (USERGROUP & 2) {
-
-	if (output4State = "off") {
-		$("#greenbtn").attr('class', 'button');
-	} else {
-		$("#greenbtn").attr('class', 'button2');
-		
-	}
-	
-} else {
-		$("#greenbtn").attr('class', 'btnhide');
-}
-
-if (USERGROUP & 4) {
-	if (output5State = "off") {
-		$("#redbtn").attr('class', 'button');
-	} else {
-		$("#redbtn").attr('class', 'button2');
-		
-	}
-	
-} else {
-		$("#redbtn").attr('class', 'btnhide');
-}
-
+function updateButtons() {
+	$.getJSON( "/user_info", function( data ) {
+		btnControl(data.group);
+	})
 }
