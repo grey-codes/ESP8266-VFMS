7659c8e7b35c5eb045f877408e08768e0bd9dd49
Grey Ruessler <greyruessler@gmail.com>
2019-11-19 22:53:52 -0600

    fix tab active

--- data/tabhandler.js
+++ data/tabhandler.js
@@ -2,5 +2,5 @@ function openForm(formName) {
 	$(".tabcontent").css("display","none");
 	$(".tablinks").removeClass("active");
 	$("#"+formName).css("display","block");
-	$("#"+formName+" Tab").addClass("active");
+	$("#"+formName+"_Tab").addClass("active");
 } 
