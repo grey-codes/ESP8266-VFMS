523a5fe72e3ef3da78bb44603e91bef812633233
Grey Ruessler <greyruessler@gmail.com>
2019-11-19 22:49:19 -0600

    overhaul tab display with jquery

--- data/tabhandler.js
+++ data/tabhandler.js
@@ -1,20 +1,6 @@
-function openForm(form, formName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(formName).style.display = "block";
-  form.currentTarget.className += " active";
+function openForm(formName) {
+	$(".tabcontent").css("display","none");
+	$(".tablinks").removeClass("active");
+	$("#"+formName).css("display","block");
+	$("#"+formName+" Tab").addClass("active");
 } 
